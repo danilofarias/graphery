990f079078f82e2690666dbf99450b0567421370
Carlos Vinícius <me@carlosvincius.com>
2016-05-21 18:32:38 -0300

    property name fixed

--- create.js
+++ create.js
@@ -2,7 +2,7 @@ var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://neo4j:neo4j@localhost:7474');
 
 db.cypher({
-    query: "CREATE (account:Microservice { name: 'Test' })",
+    query: "CREATE (test:Microservice { name: 'Test' })",
     params: {}
 }, function (err, results) {
     if (err) throw err;
