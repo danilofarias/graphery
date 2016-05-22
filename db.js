f79558436a194c40f314e1c8b46fa380dee79def
Vitor Teixeira <vitorfox@gmail.com>
2016-05-22 15:35:44 -0300

    Can setup db using env vars

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 var neo4j = require('neo4j');
 var fs = require('fs');
 
-var db = new neo4j.GraphDatabase('http://neo4j:12345@localhost:7474');
+var dbuser = process.env.DB_USER || "neo4j";
+var dbpass = process.env.DB_PASS || "neo4j";
+var dbhost = process.env.DB_HOST || "localhost";
+var dbport = process.env.DB_HOST || "7474";
+
+var db = new neo4j.GraphDatabase('http://'+dbuser+':'+dbpass+'@'+dbhost+':'+dbport);
 
 fs.readFile( __dirname + '/data.cypher', function (err, data) {
     if (err) throw err;
