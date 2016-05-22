bc06cc85493c7df038c04858515bbda9be7e7a95
Danilo Farias <danilodsfarias@gmail.com>
2016-05-22 18:52:54 -0300

    Show node labels

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,22 @@
-var width = 960,
+// Graph
+var width = 980,
     height = 500;
 
+var color = d3.scale.category20();
+
 var force = d3.layout.force()
-    .charge(-200)
-    .linkDistance(80)
+    .charge(-120)
+    .linkDistance(150)
     .size([width, height]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".graph-container").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 d3.json("/info", function(error, services) {
   if (error) throw error;
 
+  // Format json output 
   var tmpnodes = [];
   var links = [];
 
@@ -36,42 +40,50 @@ d3.json("/info", function(error, services) {
 
   var graph = {nodes: nodes, links: links};
 
-  var nodeById = d3.map();
+  // Graph setup  
+  var drawGraph = function(graph) {
 
-  graph.nodes.forEach(function(node) {
-    nodeById.set(node.id, node);
-  });
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
-  graph.links.forEach(function(link) {
-    link.source = nodeById.get(link.source);
-    link.target = nodeById.get(link.target);
-  });
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+      .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+    var gnodes = svg.selectAll('g.gnode')
+       .data(graph.nodes)
+       .enter()
+       .append('g')
+       .classed('gnode', true);
+      
+    var node = gnodes.append("circle")
+        .attr("class", "node")
+        .attr("r", 18)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link");
+    // Labels
+    var labels = gnodes.append("text")
+        .text(function(d) { return d.name; });
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 18)
-      .style("fill", function(d) { return d.id; })
-      .call(force.drag);
+    // Nodes position
+    force.on("tick", function() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+      
+      gnodes.attr("transform", function(d) { 
+          return 'translate(' + [d.x, d.y] + ')'; 
+      });
+          
+    });
+  };
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+  drawGraph(graph);
 
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
 });
