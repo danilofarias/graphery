28c4a200d11e5c4f4243546589b84f988a72ff7d
Carlos Vinicius <me@carlosvinicius.com>
2016-05-22 16:16:58 +0000

    parse function added to read servers json and d3 integration

--- src/js/main.js
+++ src/js/main.js
@@ -1,126 +1,77 @@
-var services = [
-    {
-        "b": {
-            "_id": 188,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Account"
-            }
-        },
-        "r": {
-            "_id": 265,
-            "type": "DEPENDS_ON",
-            "properties": {},
-            "_fromId": 185,
-            "_toId": 188
-        },
-        "services": {
-            "_id": 185,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Auth"
-            }
-        }
-    },
-    {
-        "b": {
-            "_id": 187,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Search"
-            }
-        },
-        "r": {
-            "_id": 264,
-            "type": "DEPENDS_ON",
-            "properties": {},
-            "_fromId": 186,
-            "_toId": 187
-        },
-        "services": {
-            "_id": 186,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Chat"
-            }
-        }
-    },
-    {
-        "b": {
-            "_id": 188,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Account"
-            }
-        },
-        "r": {
-            "_id": 263,
-            "type": "DEPENDS_ON",
-            "properties": {},
-            "_fromId": 186,
-            "_toId": 188
-        },
-        "services": {
-            "_id": 186,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Chat"
-            }
-        }
-    },
-    {
-        "b": {
-            "_id": 185,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Auth"
-            }
-        },
-        "r": {
-            "_id": 262,
-            "type": "DEPENDS_ON",
-            "properties": {},
-            "_fromId": 186,
-            "_toId": 185
-        },
-        "services": {
-            "_id": 186,
-            "labels": [
-                "Microservice"
-            ],
-            "properties": {
-                "name": "Chat"
-            }
-        }
+var width = 960,
+    height = 500;
+
+var force = d3.layout.force()
+    .charge(-200)
+    .linkDistance(80)
+    .size([width, height]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("data/services.json", function(error, services) {
+  if (error) throw error;
+
+  var tmpnodes = [];
+  var links = [];
+
+  services.forEach(function(e) {
+    var target = e.b._id;
+    var source = e.services._id;
+    links.push({source: source, target: target});
+
+    tmpnodes.push({"id": target, "name": e.b.properties.name});
+    tmpnodes.push({"id": source, "name": e.services.properties.name});
+  });
+
+  var exists = [];
+  var nodes = [];
+  tmpnodes.forEach(function(e) {
+    if (exists[e.id] == undefined) {
+      nodes.push(e);
+      exists[e.id] = true;
     }
-];
+  });
 
-console.log(services);
+  var graph = {nodes: nodes, links: links};
 
-var output = {
-    "nodes": [{
-        "id": "chat"
-    }, {
-        "id": "auth"
-    }],
-    "links": [{
-        "source": "chat",
-        "target": "auth"
-    }]
-}
+  var nodeById = d3.map();
 
-console.log(output);
+  graph.nodes.forEach(function(node) {
+    nodeById.set(node.id, node);
+  });
+
+  graph.links.forEach(function(link) {
+    link.source = nodeById.get(link.source);
+    link.target = nodeById.get(link.target);
+  });
+
+  force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(graph.links)
+    .enter().append("line")
+      .attr("class", "link");
+
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 18)
+      .style("fill", function(d) { return d.id; })
+      .call(force.drag);
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+});
